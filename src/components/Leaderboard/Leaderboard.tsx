c0fc31f79cb49d00ff97ff250aa3962f242dc91e
ribgu <gustavo.ribeiro@sptech.school>
2024-04-28 22:17:24 -0300

    começa leaderboard

---
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -0,0 +1,17 @@
+import Box from '@mui/material/Box/Box'
+import Typography from '@mui/material/Typography/Typography'
+
+export default function Leaderboard () {
+    return (
+        <Box>
+            <Box>
+                <Box>
+                    icone de trofeu
+                    <Typography>Leaderboard</Typography>
+                </Box>
+                <Typography>Alunos que mais acertaram questões</Typography>
+            </Box>
+            <Box></Box>
+        </Box>
+    )
+}
