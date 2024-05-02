ab4696780062ea4242c2900e6c601dcb86b0fdd9
ribgu <gustavo.ribeiro@sptech.school>
2024-05-01 21:11:20 -0300

    add header

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,18 +1,23 @@
 import Box from '@mui/material/Box/Box'
 import Typography from '@mui/material/Typography/Typography'
 
-export default function Leaderboard () {
+export default function Leaderboard() {
     return (
-        <Box>
-            <Box>
-                <Box>
-                    icone de trofeu
-                    <Typography>Leaderboard</Typography>
+        <Box sx={{ backgroundColor: 'red', display: 'flex', width: '20vw', justifyContent: 'center', alignItems: 'center' }}>
+            <Box sx={{}}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    icon
+                    <Typography sx={{ fontSize: '16px', fontWeight: '800' }}>Leaderboard</Typography>
                 </Box>
-                <Typography>Alunos que mais acertaram questões</Typography>
+                <Typography sx={{ fontSize: '12px' }}>Alunos que mais acertaram questões</Typography>
+                <Box sx={{ display: 'flex', gap: '10px' }}>
+                    <Typography sx={{ fontSize: '12px' }}>#</Typography>
+                    <Typography sx={{ fontSize: '12px' }}>Nome</Typography>
+                    <Typography sx={{ fontSize: '12px' }}>Questões</Typography>
+                </Box>
             </Box>
             <Box>
-                
+
             </Box>
         </Box>
     )
--- src/pages/Teste.tsx
+++ src/pages/Teste.tsx
@@ -1,15 +1,11 @@
-import  TextField  from '@mui/material/TextField'
-import Modal from '../components/Modal/Modal'
+import LeaderBoard from '../components/Leaderboard/Leaderboard'
 
 export default function Teste() {
   return (
     <div style={{
       width: '500px'
     }}>
-      <Modal variantButton='sm' titulo='Nova turma' icone='../../public/IconsPages/turma.svg' altIcone='Pessoas agrupadas' textoBotaoAbrirModal='Nova Turma' textoBotaoConfirmar='Criar'>
-        <TextField variant='outlined' label='Nome*'/>
-        <TextField variant='outlined' label='Disciplina*'/>
-      </Modal>
+      <LeaderBoard />
     </div>
   )
 }
